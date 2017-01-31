d1991b8f7f8db4bb433ffb0a359d0c7603ab8d11
THE27-SRV\mityankin <m.mitjankin@gmail.com>
2017-01-31 08:23:34 +0200

    no message

---
+++ WebFormCalcMityankin/Default.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebFormCalcMityankin.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    </head>
+
+<body style="height: 410px">
+    <form id="form1" runat="server">
+            
+        <asp:Label ID="CalcHistory" runat="server"  Width="280pt"></asp:Label>
+        <asp:Label ID="Label1" runat="server" Text="=" Width="20pt"></asp:Label>
+        <asp:Label ID="Result" runat="server" Width="100pt"></asp:Label>
+        <br/>
+        <asp:Button ID="Clear" runat="server"  Width="306pt" Text="C" OnClick="Clear_Click" />
+        <asp:Button ID="Multiply" runat="server" Width="100pt" Text="*" OnClick="Multiply_Click" />
+        <br/>
+        <asp:Button ID="Num7" runat="server" Width="100pt" Text="7" CommandArgument="7" OnClick="Num_Clic" />
+        <asp:Button ID="Num8" runat="server" Width="100pt" Text="8" CommandArgument="8" OnClick="Num_Clic" />
+        <asp:Button ID="Num9" runat="server" Width="100pt" Text="9" CommandArgument="9" OnClick="Num_Clic" />
+        <asp:Button ID="Divided" runat="server" Width="100pt" Text="/" OnClick="Divided_Click" />
+        <br/>
+        <asp:Button ID="Num4" runat="server" Width="100pt" Text="4" CommandArgument="4" OnClick="Num_Clic" />
+        <asp:Button ID="Num5" runat="server" Width="100pt" Text="5" CommandArgument="5" OnClick="Num_Clic" />
+        <asp:Button ID="Num6" runat="server" Width="100pt" Text="6" CommandArgument="6" OnClick="Num_Clic" />
+        <asp:Button ID="Minus" runat="server" Width="100pt" Text="-" OnClick="Minus_Click" />
+        <br/>
+        <asp:Button ID="Num1" runat="server" Width="100pt" Text="1" CommandArgument="1" OnClick="Num_Clic" />
+        <asp:Button ID="Num2" runat="server" Width="100pt" Text="2" CommandArgument="2" OnClick="Num_Clic" />
+        <asp:Button ID="Num3" runat="server" Width="100pt" Text="3" CommandArgument="3" OnClick="Num_Clic" />
+        <asp:Button ID="Add" runat="server" Width="100pt" Text="+" OnClick="Add_Click" />
+        <br/>
+        <asp:Button ID="Nm0" runat="server" Width="100pt" Text="0" CommandArgument="0" OnClick="Num_Clic" />
+        <asp:Button ID="Done" runat="server" Width="306pt" Text="=" OnClick="Done_Click" />
+        <br/>              
+    </form>
+    </body>
+</html>
---
+++ WebFormCalcMityankin/Default.aspx.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebFormCalcMityankin
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        private string _previousValue;
+        private string _currentValue;
+        private string _act;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["PreviousValue"] != null)
+            {
+                _previousValue = Session["PreviousValue"].ToString(); 
+            }
+            if (Session["CurrentValue"] != null)
+            {
+                _currentValue = Session["CurrentValue"].ToString(); 
+            }
+            if (Session["Act"] != null)
+            {
+                _act = Session["Act"].ToString();
+            }
+            if (_previousValue == null && _currentValue == null && _act == null)
+            {
+                this.Result.Text = "";
+            }
+        }
+
+          protected void Num_Clic(object sender, EventArgs e)
+        {
+            var b = (sender as Button);
+            this.CalcHistory.Text = this.CalcHistory.Text + b.CommandArgument;
+            Session["CurrentValue"] = (_currentValue != null) ? _currentValue + b.CommandArgument : b.CommandArgument;
+        }
+
+        protected void Nm0_Click(object sender, EventArgs e)
+        {
+            this.CalcHistory.Text = this.CalcHistory.Text + "0";
+            
+            Session["CurrentValue"] = (_currentValue != null) ? _currentValue + "0" : "0";
+        }
+
+
+
+        protected void Clear_Click(object sender, EventArgs e)
+        {
+            this.CalcHistory.Text = "";
+            this.Result.Text = "";
+            Session["CurrentValue"] = null;
+            Session["Act"] = null;
+            Session["PreviousValue"] = null;
+        }
+
+        protected void Multiply_Click(object sender, EventArgs e)
+        {
+            if(_currentValue != null)
+            {
+                this.CalcHistory.Text = this.CalcHistory.Text + "*";
+                CalcAction();
+                Session["Act"] = "multiply";
+            }
+
+        }
+
+        protected void Divided_Click(object sender, EventArgs e)
+        {
+            if (_currentValue != null)
+            {
+                this.CalcHistory.Text = this.CalcHistory.Text + "/";
+                CalcAction();
+                Session["Act"] = "divided";
+            }
+
+        }
+
+        protected void Minus_Click(object sender, EventArgs e)
+        {
+           
+            if (_previousValue == null && _currentValue == null && _act == null)
+            {
+                this.CalcHistory.Text = this.CalcHistory.Text + "-";
+                Session["CurrentValue"] =  "-";
+            }
+            else if(_currentValue!=null && !_currentValue.Equals("-"))
+            {
+                this.CalcHistory.Text = this.CalcHistory.Text + "-";
+                CalcAction();
+                Session["Act"] = "minus";
+            }
+
+
+        }
+
+        protected void Add_Click(object sender, EventArgs e)
+        {
+            if (_currentValue != null)
+            {
+                this.CalcHistory.Text = this.CalcHistory.Text + "+";
+                CalcAction();
+                Session["Act"] = "plus";
+            }
+
+        }
+
+        protected void Done_Click(object sender, EventArgs e)
+        {
+            CalcAction();
+            this.CalcHistory.Text = "";
+            Session["CurrentValue"] = null;
+            Session["Act"] = null;
+            Session["PreviousValue"] = null;
+        }
+
+
+        private void CalcAction()
+        {
+            if (_previousValue != null && _currentValue != null && _act != null)
+            {
+                string result = CalcValue(_previousValue, _currentValue);
+                this.Result.Text = result;
+                Session["CurrentValue"] = null;
+                Session["PreviousValue"] = result;
+
+            }
+            else if (_currentValue != null && _previousValue == null)
+            {
+                Session["PreviousValue"] = _currentValue;
+                Session["CurrentValue"] = null;
+            }
+
+
+
+        }
+
+        private string CalcValue(string pre, string curr)
+        {
+            string returnValue = "";
+            int result;
+            try
+            {
+                int a = Int32.Parse(pre);
+                int b = Int32.Parse(curr);
+                switch (_act)
+                {
+                    case "multiply":
+                        result = a * b;
+                        returnValue = result.ToString();
+                        break;
+                    case "divided":
+                        if (b != 0)
+                        {
+                            result = a / b;
+                            returnValue = result.ToString();
+                        }
+                        else
+                        {
+                            returnValue = "Error";
+                        }
+                        break;
+                    case "minus":
+                        result = a - b;
+                        returnValue = result.ToString();
+                        break;
+                    case "plus":
+                        result = a + b;
+                        returnValue = result.ToString();
+                        break;
+
+                }
+
+            }
+            catch(Exception e)
+            {               
+                returnValue = "Something went wrong";
+            }
+
+            return returnValue;
+        }
+
+    }
+}
